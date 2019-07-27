7e1684519ba397dbb9bd0376d8c806ce92328e33
Anthony Ng <newyork.anthonyng@gmail.com>
2019-07-27 17:01:28 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: path.resolve(__dirname, "src/index.js"),
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js"
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                        ],
+                        plugins: [
+                            "@babel/plugin-transform-react-jsx"
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlPlugin({
+            template: path.resolve(__dirname, "src/index.html")
+        })
+    ]
+}
